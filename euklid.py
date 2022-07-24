f50b15ed553ab25b8b4841a58f90b0e9bd25e128
Jannik Schmied <jannik.schmied@pm.me>
2022-07-24 22:12:43 +0200

    added euklids ggt algorithms and euklids extended algorithm

---
+++ euklid.py
@@ -0,0 +1,48 @@
+def euklid_ggT(n: int, a: int) -> int:
+	if n == 0 or a == 0:
+		return 0
+	
+	while a != 0:
+		a, n = n % a, a
+		
+	return n
+		
+
+def extended_euklid(n: int, a: int):
+	c: int
+	d: int
+	uc: int
+	vc: int
+	ud: int
+	vd: int
+	q: int
+	
+	c, d = a, n
+	uc, vc, ud, vd = 1, 0, 0, 1
+	
+	while c != 0:
+		q = int(d/c)
+		c, d = d - q * c, c
+		uc, vc, ud, vd = ud - q * uc, vd - q * vc, uc, vc
+		
+	k, v, u = d, vd, ud
+	
+	return k, v, u
+
+
+def main():
+	a: int = 3
+	b: int = 6
+	c: int = 17
+	
+	print(euklid_ggT(a, b))
+	print(euklid_ggT(b, c))
+	
+	print(extended_euklid(c, a))
+	
+	exit(0)
+	
+	
+if __name__ == '__main__':
+	main()
+
