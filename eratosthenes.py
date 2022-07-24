6f4a5299974a9a82a789b455da55a0a94589ee11
Jannik Schmied <jannik.schmied@pm.me>
2022-07-24 22:12:00 +0200

    added sieve of eratostehenes

---
+++ eratosthenes.py
@@ -0,0 +1,32 @@
+from math import sqrt
+
+
+def sieve_of_eratosthenes(upper_limit: int) -> list:
+	primes: list = list()
+	
+	# Fill list with values from 2 to upper_limit
+	for i in range(2, upper_limit + 1):
+		primes.append(i)
+						
+	sieving_limit = int(sqrt(upper_limit))
+	
+	for n in range(2, sieving_limit + 1):
+		for i in primes:
+			if i % n == 0:
+				if i != n:
+					primes.remove(i)
+				
+	return primes
+
+
+def main():
+	range: int = 5
+	
+	print(sieve_of_eratosthenes(range))
+	
+	exit(0)
+	
+	
+if __name__ == '__main__':
+	main()
+
