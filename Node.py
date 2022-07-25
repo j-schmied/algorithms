3a4e5c8f7b5c64a41a7909f1ca22c2c04429f5b0
JAnx0 <jannik.schmied@pm.me>
2022-07-25 10:36:04 +0200

    Some corrections

--- Node.py
+++ Node.py
@@ -1,26 +1,41 @@
-class Node:
-	
-	def __init__(self, value: int, left_child: Node = None, right_child: Node = None):
-		self._left_child: Node = left_child
-		self._right_child: Node = right_child
-		self._value = value
+from typing import Generic, TypeVar
 
-	def add_left_child(self, left: Node)
-		if self.left_child == None:
-			self._left_child = left
+Node = TypeVar('Node', bound='Node')
 
-	def add_right_child(self, right: Node):
-		if self._right_child == None:
-			self._right_child = right
-			
-	def remove_left_child(self):
-		if self._right_child == None:
-			self._left_child = None
-		
-		if self._right_child not None:
-			self._left_child = self.right_child
-			self.remove_right_child()
-			
-	def remove_right_child(self):
-		if self._right_child not None:
-			self._right_child = None 	
+class Node(Generic[Node]):
+    def __init__(self, value: int, left_child: Node = None, right_child: Node = None):
+        self._left_child: Node = left_child
+        self._right_child: Node = right_child
+        self._value = value
+
+    def add_left_child(self, left: Node):
+        if self._left_child == None:
+            self._left_child = left
+
+    def add_right_child(self, right: Node):
+        if self._right_child == None:
+            self._right_child = right
+
+    def remove_left_child(self):
+        if self._right_child == None:
+            self._left_child = None
+
+        if self._right_child != None:
+            self._left_child = self.right_child
+            self.remove_right_child()
+
+    def remove_right_child(self):
+        if self._right_child != None:
+            self._right_child = None
+
+    @property
+    def value(self) -> int:
+        return self._value
+
+    @property
+    def left_child(self) -> Node:
+        return self._left_child
+
+    @property
+    def right_child(self) -> Node:
+        return self._right_child
